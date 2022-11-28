d907f327a5e5c84e7012c773ed9fcab0869b9cf2
Michael Berger <m.gonser@icloud.com>
2022-11-28 17:35:28 +0100

    add guards package

---
+++ packages/ts-kit/guards/src/index.ts
@@ -0,0 +1,6 @@
+export { isArray } from './lib/is-array';
+export { isBoolean } from './lib/is-boolean';
+export { isNumber } from './lib/is-number';
+export { isString } from './lib/is-string';
+export { isObject } from './lib/is-object';
+export { isFile } from './lib/is-file';
